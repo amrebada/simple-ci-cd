29203bef600f7fe9353d43d0783bf310250609fc
amr ebada <amr.ebada@tradeling.com>
2022-05-15 04:56:31 +0200

    feat: add ssh agent to the clone url

--- core/github.go
+++ core/github.go
@@ -1,12 +1,13 @@
 package core
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
 
 func Clone(src, dest string) error {
-	cmd := exec.Command("git", "clone", src, dest)
+	cmd := exec.Command("ssh-agent", "bash", "-c", fmt.Sprintf("ssh-add %s; git clone %s %s", os.Getenv("SSH_KEY_PATH"), src, dest))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
